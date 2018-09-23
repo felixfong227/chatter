8ace928e0894d551effb08660b5c69b5f0803aa6
Felix Fong <moongod101@hotmail.com>
2018-09-24 00:16:50 +0800

    Create a work in progress Input element

---
+++ client/src/Components/ChatBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Input from '@material-ui/core/Input';
+
+export default class ChatBox extends React.Component {
+    render() {
+        return (
+            <Input defaultValue="It works!" />
+        );
+    }
+}
